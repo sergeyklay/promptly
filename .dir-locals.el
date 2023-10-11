2255fa8918837e361754399670d6c0ebc8d78796
Serghei Iakovlev <egrep@protonmail.ch>
2023-10-11 22:44:54 +0200

    feat: migrate to SASS

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,12 +3,9 @@
 
                  ;; Use spaces instead of tabs
 ((python-mode . ((indent-tabs-mode . nil)
-
                  ;; Set tab width to 4 spaces
                  (tab-width . 4)
-
                  ;; Set fill column to 79 chars as per PEP 8
                  (fill-column . 79)
-
                  ;; Set python indentation to 4 spaces
-                 (python-indent-offset . 4)))
+                 (python-indent-offset . 4))))
