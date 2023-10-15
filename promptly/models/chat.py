33a1ce0c6dd3263c59c64e639f8741be0a8bf00d
Serghei Iakovlev <egrep@protonmail.ch>
2023-10-15 22:43:07 +0200

    docs: amend models documentation

--- promptly/models/chat.py
+++ promptly/models/chat.py
@@ -5,11 +5,22 @@
 # For the full copyright and license information, please view
 # the LICENSE file that was distributed with this source code.
 
-from sqlalchemy import orm as so
-import sqlalchemy as sa
+"""The Chat models package for the application.
+
+This module contains the SQLAlchemy models for handling chat conversations
+within the application, including the :class:`.Chat` and
+:class:`.ChatEntry` models.
+
+.. note:: This module assumes the existence of a SQLAlchemy database session,
+          and the necessary base mixins for identity and timestamp handling.
+"""
+
 from typing import List
 
-from .base import BaseMixin, IdentityMixin, TimestampMixin, db
+import sqlalchemy as sa
+from sqlalchemy import orm as so
+
+from .base import BaseMixin, db, IdentityMixin, TimestampMixin
 
 
 class Chat(BaseMixin, IdentityMixin, TimestampMixin, db.Model):
