a6f3bb15c788dc56260e4a2153a08880abaaec20
Serghei Iakovlev <egrep@protonmail.ch>
2023-10-23 09:18:23 +0200

    test: amend chat tests

--- tests/js/chat.test.js
+++ tests/js/chat.test.js
@@ -5,12 +5,58 @@
 // For the full copyright and license information, please view
 // the LICENSE file that was distributed with this source code.
 
-import { createChatElement } from '../../promptly/static/js/chat.js';
+import {
+    createChatElement,
+    autoResizePromptTextarea
+} from '../../promptly/static/js/chat.js';
 
-describe('chat', () => {
+describe('chat-elements', () => {
     test('should create a chat element with given message and role', () => {
         const element = createChatElement('Hello', 'user');
         expect(element.getAttribute('data-entry-role')).toBe('user');
         expect(element.textContent).toBe('Hello');
+    });
+});
+
+describe('chat-textarea', () => {
+    let originalGetComputedStyle;
+    let textarea;
+
+    beforeEach(() => {
+        originalGetComputedStyle = window.getComputedStyle;
+        textarea = document.createElement('textarea');
+        document.body.appendChild(textarea);
+    });
+
+    afterEach(() => {
+        window.getComputedStyle = originalGetComputedStyle;
+        document.body.removeChild(textarea);
+    });
+
+    const createMockElement = (scrollHeight) => ({
+        style: {},
+        scrollHeight,
+        setAttribute: jest.fn(),
+    });
+
+    const mockGetComputedStyle = (value) => {
+        window.getComputedStyle = jest.fn().mockReturnValue({
+            lineHeight: value,
+        });
+    };
+
+    test('should update height based on scrollHeight', () => {
+        mockGetComputedStyle('20px');
+
+        const mockElement = createMockElement(50);
+        autoResizePromptTextarea(mockElement);
+
+        expect(mockElement.style.height).toBe('50px');
+    });
+
+    test('should set overflowY to hidden', () => {
+        autoResizePromptTextarea(textarea);
+
+        expect(textarea.style.overflowY).toBe('hidden');
     });
 });
