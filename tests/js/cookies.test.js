86ee7f939345882af0b98c473c87c76089585c72
Serghei Iakovlev <egrep@protonmail.ch>
2023-10-23 08:53:23 +0200

    test: add cookies test

---
+++ tests/js/cookies.test.js
@@ -0,0 +1,19 @@
+// This file is part of the Promptly.
+//
+// Copyright (C) 2023 Serghei Iakovlev <egrep@protonmail.ch>
+//
+// For the full copyright and license information, please view
+// the LICENSE file that was distributed with this source code.
+
+import { getCookie, setCookie } from '../../promptly/static/js/cookie.js';
+
+describe('cookies', () => {
+    test('should set and get a cookie', () => {
+        setCookie('test', 'value', 1);
+        expect(getCookie('test')).toBe('value');
+    });
+
+    test('should return null if cookie not found', () => {
+        expect(getCookie('nonexistent')).toBe(null);
+    });
+});
